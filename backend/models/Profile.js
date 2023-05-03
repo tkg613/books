50c1005a78d5bb6378b218b6aaa1112dd2b3ab61
tkg613 <lyntallow@mail.com>
2023-05-03 09:21:23 +0900

    Add profile model

---
+++ backend/models/Profile.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+
+const profileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
+  },
+  books: {
+    type: [String]
+  },
+  bio: {
+    type: String,
+  },
+  experience: {
+    title: {
+      type: String,
+      required: true
+    },
+    from: {
+      type: Date,
+      required: true,
+    },
+    to: {
+      type: Date
+    },
+    current: {
+      type: Boolean,
+      default: false
+    },
+    description: {
+      type: String
+    }
+  },
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedIn: {
+      type: String,
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = User = mongoose.model('profile', profileSchema)
